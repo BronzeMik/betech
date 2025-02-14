6fffa8afb58579b91b6a32dde8f0159471991a3b
bronzemik <mbr2201@wgu.edu>
2025-02-14 16:17:57 -0500

    update sitemap

--- next-sitemap.js
+++ next-sitemap.js
@@ -1,5 +1,8 @@
-module.exports = {
-    siteUrl: "https://betechpro.com",
-    generateRobotsTxt: true,
+/** @type {import('next-sitemap').IConfig} */
+export default {
+    siteUrl: "https://betechpro.com", 
+    generateRobotsTxt: true, 
+    generateIndexSitemap: false,
+    sitemapSize: 5000, 
   };
   
